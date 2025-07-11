43920fc7d35807d8aeef7b671d3ea02514d5ab17
666sul <1546782278@qq.com>
2025-07-11 20:58:18 +0800

    7/11

---
+++ temperature1.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+
+
+
+int Forachieve (void)
+{
+	int fahr ;
+	for(fahr=0;fahr<=300;fahr=fahr+20)
+	
+		printf("%3d %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+}
+
+
+int  Forachnixu(void)
+{
+	int  fahr;
+	for(fahr=300;fahr>=0;fahr=fahr-20)
+		printf("%3d %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+}
+
+
+
+
