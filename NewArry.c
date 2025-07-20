8137970f61be3930cb3ba5701cf7eb8570628308
666sul <1546782278@qq.com>
2025-07-20 23:25:11 +0800

    7/20

---
+++ NewArry.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+int main ()
+{
+
+//========================冒泡排序
+	/*int p,q,s,i,j;
+	int a[10];
+	printf("Put into Numbre : \n");
+	for( i = 0;i<10; i++)
+	{	
+		scanf("%d",&a[i]);
+	}
+
+	for ( i=0 ;i<10;i++)
+	{
+		p = i;
+		q = a[i];
+		for(j = i+1;j<10;j++)
+			if(q<a[j])
+			{
+				p=j;
+				q=a[j];	
+			}
+
+	if(i!=p)
+	{
+		s=a[i];
+		a[i]=a[p];		
+		a[p]=s;
+	}
+	
+	printf("%d ",a[i]);
+           
+	}*/
+//=================================二维数组=========
+
+
+	/*int i ,j ;
+	char a [][5] ={
+			{'h', 'e', 'l','l','o'},
+			{'w','o','r','d',  '!'}
+	      	      };
+
+
+	for(i = 0; i<=1;i++)
+	{
+		for(j=0;j<5;j++)
+			printf(" %c ",a[i][j]) ;
+
+
+	}*/
+//================================================
+
+/*	char a [] ={"hello,world!"};
+	printf("%s\n",a);	
+	char st [15];
+	printf("\n input to string :");
+	scanf("%s",st);
+	printf("%s\n",st);	
+
+	char st1[16],st2[6],st3[6],st4[6];
+
+	printf("input string  :\n");
+	scanf("%s %s %s %s",st1,st2,st3,st4);
+	printf("%s%s%s%s\n",st1,st2,st3,st4);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
